8d9d95e1a87521d4efff0c23a9cd33f8df74a1b1
YaroslavBukhgolts <55090332+YaroslavBukhgolts@users.noreply.github.com>
2023-08-20 23:59:14 +0300

    Lesson 4 - Vuetify и другие готовые шаблоны
    В качаестве готовых шаблонов использовал библиотеку ant-design-vue из-за несовместимости Vuetify с 3-ей версией Vue.js

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <a-layout class="layout">
+    <!-- HEADER -->
+    <a-layout-header>
+      <a-button type="primary" href="/">Home</a-button>
+      <a-button type="primary" href="/about">About</a-button>
+    </a-layout-header>
+
+    <!-- CONTENT -->
+    <a-layout-content style="padding: 0 50px">
+      <div :style="{ background: '#fff', padding: '24px', minHeight: '280px' }">Content</div>
+      <RouterView />
+    </a-layout-content>
+
+    <!-- FOOTER -->
+    <a-layout-footer style="text-align: center">
+      Ant Design ©2018 Created by Ant UED
+    </a-layout-footer>
+  </a-layout>
+</template>
+
+<style>
+  .layout {
+    min-height: 100vh;
+    width: 100%;
+  }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  props: ['msg'],
+  data(){
+    return {
+      timerId: -1,
+      currentTextIndex: 0,
+      text: '',
+      greetingText:[
+        'You’ve successfully created a project with1',
+        'You’ve successfully created a project with2',
+        'You’ve successfully created a project with3',
+        'You’ve successfully created a project with4',
+        'You’ve successfully created a project with5',
+      ],
+    }
+  },
+  mounted(){
+    this.text = this.greetingText[0];
+    this.timerId = setTimeout(this.changeText, 3000);
+  },
+  unmounted(){
+    clearTimeout(this.timerId);
+  },
+  methods:{
+    changeText(){
+      clearTimeout(this.timerId);
+      this.currentTextIndex++;
+      if(this.currentTextIndex == this.greetingText.length) this.currentTextIndex = 0;  
+      this.text = this.greetingText[this.currentTextIndex];
+      this.timerId = setTimeout(this.changeText, 3000);
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green" @click="changeText">{{ msg }}</h1>
+    <h3>
+      {{ timerId }}
+      {{ text }}
+    </h3>
+  </div>
+</template>
